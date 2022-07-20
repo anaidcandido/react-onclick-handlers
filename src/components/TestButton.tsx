929b31f7bc9d95250881e2bc85740afbda92d3cb
Anaid Candido <anaid.candido@gmail.com>
2022-07-20 19:07:25 +0100

    add function to button

--- src/components/TestButton.tsx
+++ src/components/TestButton.tsx
@@ -1,5 +1,21 @@
-function TestButton(): JSX.Element {
-  return <button>Make me do something when I'm clicked...</button>;
-}
+//function TestButton(): JSX.Element {
+//  function handleClick() {
+//    console.log("This is only a Test Button");
+//  }
+//  return (
+//    <button onClick={handleClick}>
+//      Make me do something when I'm clicked...
+//    </button>
+//  );
+//}
 
 export default TestButton;
+
+function TestButton():JSX.Element{
+  return (
+    <button onClick={() => window.open("a new function")}
+    >
+        Make me open a new window
+    </button>
+  )
+} 
